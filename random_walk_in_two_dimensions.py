aebe17e12b9cf5431564005296a20113e7c6da38
= <=>
2021-12-21 14:39:06 -0500

    added two dimensional diffusion

---
+++ random_walk_in_two_dimensions.py
@@ -0,0 +1,22 @@
+import matplotlib.animation as ani
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+steps = int(input("enter number of steps"))
+
+def create_single_particle_diffusion(steps):
+    # the probability that we move (or dont move) in any direction is equal
+    prob_left, prob_right, prob_up, prob_down, prob_stay = .25
+
+    # create a list of pairs representing position in x and y at time t
+    position = [[0,0]] # start a x = y = 0
+    for N in range(1, steps):
+        previous_position_x = position[i-1][0]
+        previous_position_y = position[i-1][1]
+
+        new_position_x = random.choice(previous_position_x, previous_position_x + 1, previous_position_x - 1)
+        new_position_y = random.choice(previous_position_y, previous_position_y + 1, previous_position_y - 1)
+
+        position.append([new_position_x, new_position_y])
+    return position
